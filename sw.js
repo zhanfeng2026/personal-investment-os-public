c0072204c3163e1c4830ff7ad7e967c4d8f9e8e7
zhanfeng2026 <286407196+zhanfeng2026@users.noreply.github.com>
2026-08-31 03:09:29 +0800

    Deploy standalone site

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE = 'investment-os-v1';
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', (event) => event.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request).then((response) => {
+    const copy = response.clone();
+    caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match(event.request).then((cached) => cached || caches.match('./'))));
+});
